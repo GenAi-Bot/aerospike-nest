06330f901ce9425b0ed3d8709fe7e1518a770a46
bit0r1n <me@bitor.in>
2025-04-28 17:48:41 +0300

    module: isGlobal value

--- src/aerospike.module-definition.ts
+++ src/aerospike.module-definition.ts
@@ -2,4 +2,14 @@ import { ConfigurableModuleBuilder } from '@nestjs/common'
 import { ConfigOptions } from 'aerospike'
 
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
-  new ConfigurableModuleBuilder<ConfigOptions>().build()
+  new ConfigurableModuleBuilder<ConfigOptions>()
+    .setExtras(
+      {
+        isGlobal: false,
+      },
+      (definition, extras) => ({
+        ...definition,
+        global: extras.isGlobal,
+      }),
+    )
+    .build()
